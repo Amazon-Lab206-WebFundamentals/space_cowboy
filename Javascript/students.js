99a4a39a5df17ab9458f7aa6660d0eb3dbde6d21
Space Cowboy <ferrante.sam@gmail.com>
2017-10-16 20:01:56 -0700

    Add files via upload

---
+++ Javascript/students.js
@@ -0,0 +1,19 @@
+var people = {
+    "students" [
+        {"name" "Sam"},
+        {"name" "Val"}
+    ],
+    "Intrusctors" [
+        {"name" "Todd"},
+        {"name" "Sean"}
+    ]
+};
+
+people.students[0].name
+
+for (i=0; i<; people.students.length; i++){
+    console.log(people.students[i].name)
+}
+
+foreach(var key in object)
+    for(arrays)
